ce48a9544e4b8995a98a4f490c4d9a978539cfa2
JKjiwon <jwkim.oa@gmail.com>
2025-12-03 18:32:04 +0900

    [#3] Test: 감정분석, 코사인유사도 호출 테스트

---
+++ src/test/java/com/todaybook/searchservice/infrastructure/gemini/GeminiEmotionAnalysisServiceTest.java
@@ -0,0 +1,24 @@
+package com.todaybook.searchservice.infrastructure.gemini;
+
+import com.todaybook.searchservice.application.EmotionAnalysisService;
+import com.todaybook.searchservice.application.dto.EmotionResult;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiEmotionAnalysisServiceTest {
+
+    @Autowired
+    private EmotionAnalysisService emotionAnalysisService;
+
+    @Test
+    void result() {
+        String text = "책 추천 \n" +
+                "사람의 성격: 차분한, 엉뚱한 \n" +
+                "부가 요구사항: 추리 소설" ;
+        EmotionResult result = emotionAnalysisService.analyze(text);
+        System.out.println("## Emotion = " + result.emotion());
+        System.out.println("## Query   = " + result.query());
+    }
+}
