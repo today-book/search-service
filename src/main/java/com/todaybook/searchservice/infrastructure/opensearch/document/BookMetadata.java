0045134328787e0eb08f83727dc311d385c74bd5
JKjiwon <jwkim.oa@gmail.com>
2025-12-11 22:37:49 +0900

    Fix: jackson 설정 변경
    - fail-on-unknown-properties: false

--- src/main/java/com/todaybook/searchservice/infrastructure/opensearch/document/BookMetadata.java
+++ src/main/java/com/todaybook/searchservice/infrastructure/opensearch/document/BookMetadata.java
@@ -1,5 +1,7 @@
 package com.todaybook.searchservice.infrastructure.opensearch.document;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record BookMetadata(String title, String author, List<String> categories) {}
